563c3bda81f4e90c04ee42323306f099de7b536c
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2021-09-30 19:34:34 +0200

    feature: added binary encoding for decimal and float values + tests

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # neural-symbolic-regression
+
+### Run the tests
+
+1. Install `pytest`
+2. Run `pytest` in the root folder
+
+Optionally, install the [VSCode extension](https://marketplace.visualstudio.com/items?itemName=LittleFoxTeam.vscode-python-test-adapter) that can help run the tests easily.
---
+++ tests/test_binary_encoder.py
@@ -0,0 +1,20 @@
+from binary_encoder import dec2bin, float2bin
+import pytest
+import numpy as np
+from torch import tensor
+
+def test_dec2bin():
+    assert np.all(dec2bin(tensor([1]), 10).numpy() == tensor([[0., 0., 0., 0., 0., 0., 0., 0., 0., 1.]]).numpy())
+    assert np.all(dec2bin(tensor([-1]), 10).numpy() == tensor([[1., 0., 0., 0., 0., 0., 0., 0., 0., 1.]]).numpy())
+    assert np.all(dec2bin(tensor([2]), 10).numpy() == tensor([[0., 0., 0., 0., 0., 0., 0., 0., 1., 0.]]).numpy())
+    assert np.all(dec2bin(tensor([-2]), 10).numpy() == tensor([[1., 0., 0., 0., 0., 0., 0., 0., 1., 0.]]).numpy())
+    assert np.all(dec2bin(tensor([200]), 10).numpy() == tensor([[0., 0., 1., 1., 0., 0., 1., 0., 0., 0.]]).numpy())
+    assert np.all(dec2bin(tensor([999]), 10).numpy() == tensor([[0., 1., 1., 1., 1., 0., 0., 1., 1., 1.]]).numpy())
+    assert np.all(dec2bin(tensor([9999]), 12).numpy() == tensor([[0., 1., 1., 1., 0., 0., 0., 0., 1., 1., 1., 1.]]).numpy())
+
+
+def test_float2bin():
+    assert np.all(float2bin(tensor([0.01]), 3, 3, 2).numpy() == tensor([[0., 0., 0., 0., 0., 0., 1.]]).numpy())
+    assert np.all(float2bin(tensor([0.01]), 7, 7, 2).numpy() == tensor([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.]]).numpy())
+    assert np.all(float2bin(tensor([0.01]), 7, 7, 2).numpy() == tensor([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.]]).numpy())
+

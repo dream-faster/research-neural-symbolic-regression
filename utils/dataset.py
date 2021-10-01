e094074eae6c0f3894f7b08ad386ffb5679486ac
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2021-10-01 16:33:06 +0200

    feat(Dataset): added SymbolicRegressionDataset

---
+++ utils/dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from torch.utils.data import Dataset
+
+class SymbolicRegressionDataset(Dataset):
+    def __init__(self, file_name):
+
+        data = pd.read_csv(file_name)
+
+        self.y = data["eqs"]
+        self.X = data.drop('eqs', axis = 1)
+        self.X = self.X[["0", "1", "2", "3", "4", "5", "6", "7", "8"]].to_numpy()
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+
+        value = self.X[idx]
+        label = self.y[idx]
+
+        return value, label
